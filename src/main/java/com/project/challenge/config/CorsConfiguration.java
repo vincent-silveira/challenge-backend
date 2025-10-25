d4956b08148631267e3ed14ea21004549a192394
vincent-silveira <code.vincentsilveira@gmail.com>
2025-10-25 16:43:32 +0530

    Updated CrossOrigin Rules

---
+++ src/main/java/com/project/challenge/config/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.project.challenge.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Value("${front_end_url}")
+    private String frontEndUrl;
+
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontEndUrl)
+                        .allowedMethods("GET", "POST", "PUT" ,"DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
--- src/main/java/com/project/challenge/controller/ChallengeController.java
+++ src/main/java/com/project/challenge/controller/ChallengeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:3000/")
 
 @RestController
 @RequestMapping("/challenge")
