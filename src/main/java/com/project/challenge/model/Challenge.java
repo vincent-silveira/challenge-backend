94450643d4a87789ab38c0cbacad0473b44af381
vincent-silveira <code.vincentsilveira@gmail.com>
2025-10-26 21:28:11 +0530

    Updated PrimaryKey Genration Type

--- src/main/java/com/project/challenge/model/Challenge.java
+++ src/main/java/com/project/challenge/model/Challenge.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Challenge {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     String month;
     String description;
